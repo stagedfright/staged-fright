42bbc3dd200476e45f4ef627d58daaeb4ba0c3f0
C.S. Jiang <christine.s.jiang@gmail.com>
2017-01-28 20:21:58 -0500

    added material ui raised button component; styles

---
+++ browser/react/components/uiElements/styles/index.js
@@ -0,0 +1,15 @@
+export default {
+	myraisedbutton: {
+		margin: '5vh 30vw',
+		lineHeight: '0vh',
+		borderRadius: '15px',
+		display: 'block',
+	},
+
+	myraisedbuttonlabel: {
+		fontFamily: 'Open Sans, sans-serif',
+		fontSize: '3vh',
+		color: 'white',
+		fontWeight: '600',
+	}
+};
