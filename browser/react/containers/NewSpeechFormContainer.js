f8a2675e3942e53f3cb45d667eca2d59b9c4555e
Harmony D <antancia@gmail.com>
2017-01-31 17:40:16 -0500

    Text now does not wrap

--- browser/react/containers/NewSpeechFormContainer.js
+++ browser/react/containers/NewSpeechFormContainer.js
@@ -11,12 +11,27 @@ const mapDispatchToProps = dispatch => ({
   submitSpeechDataForm: (fields) => {
     // evt.preventDefault();
     //fields: { wpm, speechText }
-    const speechLines = ['', '', ''].concat(fields.speechText.split('.').concat(['', '', '']))
+    function splitter(string) {
+      var arr = string.replace( /[\n\t]/g, ' ' ).split(' ');
+      var speech = [];
+      for (var i = 0; i < arr.length; i += 8) {
+        speech.push(arr.slice(i, i+8));
+      }
+      var finalSpeech = speech.map(function(lineArray) {
+        return lineArray.join(' ');
+      })
+      return finalSpeech;
+    }
+
+    const speechLines = ['3...', '2...', '1...'].concat(splitter(fields.speechText).concat(['', '', 'You\'re done!']))
     dispatch(submitSpeechData({
     	wpm: fields.wpm,
     	speechLines,
     }));
   }
 });
+
+
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(NewSpeechForm);
