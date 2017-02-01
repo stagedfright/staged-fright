9047569a0e5e7d734912d7642cddf65441294065
C.S. Jiang <christine.s.jiang@gmail.com>
2017-02-01 18:34:58 -0500

    removed socket.io

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,12 +4,10 @@ import express from 'express';
 const router = express.Router();
 
 import memeRouter from './memes';
-import trollRouter from './trolls';
 import sessionsRouter from './sessions';
 // import userRouter from './users';
 
 router.use('/memes', memeRouter);
-router.use('/trolls', trollRouter);
 router.use('/sessions', sessionsRouter);
 // router.use('/users', require('./users'));
 
