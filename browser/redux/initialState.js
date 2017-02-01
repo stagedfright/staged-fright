f3e7ee7630834832a0fb6272ee8e9dcfed905d0b
C.S. Jiang <christine.s.jiang@gmail.com>
2017-02-01 18:36:36 -0500

    updated initialstate

--- browser/redux/initialState.js
+++ browser/redux/initialState.js
@@ -3,9 +3,6 @@ import { Map } from 'immutable';
 // I prefer having a hardcoded initial state so we can have an idea of what it looks like.
 const initialState = Map({
   isInitialized: false,
-  prevLine: 0,
-  currentLine: 1,
-  nextLine: 2,
   wpm: 120,
   speechLines: [],
   locationBeforeTransitions: null
