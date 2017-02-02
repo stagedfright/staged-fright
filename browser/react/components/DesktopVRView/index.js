3da21d1c6e87a260395e656fd380b76f13c845ca
C.S. Jiang <christine.s.jiang@gmail.com>
2017-02-02 16:34:51 -0500

    added link to desktop VR view and a loading indicator for the mobile vr view hooray!

--- browser/react/components/DesktopVRView/index.js
+++ browser/react/components/DesktopVRView/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import styles from './styles';
 
 const DesktopVRView = () => (
     <div>
-    	<p>1. Allow microphone access on your desktop browser. 2. Navigate to this URL on your mobile device:</p>
+    	<h3>1. Allow microphone access on your desktop browser.<br/>2. Navigate to this URL on your mobile device: </h3>
+    	<h1>{window.location.href}</h1>
     </div>
 )
 
--- browser/react/components/VRViewer/index.js
+++ browser/react/components/VRViewer/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import styles from './styles';
-// import BasicSingleLineInput from '../BasicSingleLineInput';
 import InitialLoading from '../InitialLoading';
 import DesktopVRView from '../DesktopVRView';
 //import {Entity, Scene} from 'aframe-react';
@@ -11,42 +10,36 @@ export default class VRViewer extends Component {
   constructor(props) {
     super(props);
     console.log("PROPS", props);
-    this.state = {at: 0, time: window.performance.now()}
+    this.state = {
+      at: 0, 
+      time: window.performance.now(),
+      loading: true,
+    }
   }
 
   componentDidMount () {
-    // this.props.scrollLines(this.props.wpm, this.props.speechLines.length)
+    setTimeout(() => this.setState({ loading: false }), 1500); 
     this.tick(window.performance.now())
   }
 
   tick = time => {
     const dt = time - this.state.time
     const at = this.state.at + 0.0005 * dt
-    this.setState({time, at})
+    this.setState({ time, at })
     requestAnimationFrame(this.tick)
   }
 
   render () {
     const { handleSubmit, isInitialized } = this.props;
-    const { at } = this.state
+    const { at, loading } = this.state
     const scene = document.querySelector('a-scene');
 
     if (navigator.userAgent.match('Mobi')) {
-
-      return (
+      if (loading) {
+        return <InitialLoading />;
+      } else return (
         <div>
           <a-scene>
-
-          {/* Make the loading screen work!!
-
-            !scene.hasLoaded ?
-            <InitialLoading />
-            :
-
-
-
-          */}
-
             <a-assets>
               <video muted id="mvp" autoPlay loop src="/DT_RNC.mp4" />
             </a-assets>
@@ -55,26 +48,18 @@ export default class VRViewer extends Component {
               <a-camera>
               </a-camera>
             </a-entity>
-
-            { /*
-              <a-entity position={`0 ${-0.01 * time} 0`}>
-
-            <a-entity position=".28 5 -.26" scale="5 5 5" text={`value: ${this.props.speechLines[this.props.prevLine]}; lineHeight: 50px; opacity: 0.5`}>
-            </a-entity>
-            <a-entity position=".28 3 -.26" scale="5 5 5" text={`value: ${this.props.speechLines[this.props.currentLine]}; lineHeight: 50px;`}>
-            </a-entity>
-            <a-entity position=".28 1 -.26" scale="5 5 5" text={`value: ${this.props.speechLines[this.props.nextLine]}; lineHeight: 50px; opacity: 0.5`}>
-
-            </a-entity>
-            */ }
-
             {
-              this.props.speechLines.map((line, idx) => ({
+              this.props.speechLines
+              .map((line, idx) => ({
                 line, idx, position: [0.28, at - idx, -0.26]
               }))
-              .filter(({position: [x, y, z]}) => y > 1 && y < 5)
-              .map(({line, position, idx}) =>
-                <a-entity key={idx} position={position.join(' ')} geometry="primitive: plane; width: 100" material="side: double; transparent: true; opacity: 0; color: #EF2D5E" /*scale="5 5 5"*/ text={`value: ${line}; line-height: 30px; anchor: center; wrapCount: 1000; align: center;`} />
+              .filter(({ position: [x, y, z] }) => y > 1 && y < 5)
+              .map(({ line, position, idx }) =>
+                <a-entity key={ idx } 
+                position={ position.join(' ') } 
+                geometry="primitive: plane; width: 100" 
+                material="side: double; transparent: true; opacity: 0; color: #EF2D5E" /*scale="5 5 5"*/ 
+                text={`value: ${line}; line-height: 30px; anchor: center; wrapCount: 1000; align: center;`} />
               )
             }
           </a-scene>
@@ -83,11 +68,6 @@ export default class VRViewer extends Component {
     } else return <DesktopVRView />
   }
 }
-
-
-
-
-
 
 VRViewer.defaultProps = {
   handleSubmit: () => {
