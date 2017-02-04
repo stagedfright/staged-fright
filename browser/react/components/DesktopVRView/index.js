06d30b9dd12f69a09db71f472a6619bc62f58f65
C.S. Jiang <christine.s.jiang@gmail.com>
2017-02-04 14:38:26 -0500

    edited desktop vr view

--- browser/react/components/DesktopVRView/index.js
+++ browser/react/components/DesktopVRView/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import styles from './styles';
+import { MyNonSubmitButton } from '../uiElements';
 
-const DesktopVRView = () => (
-    <div>
-    	<h3>1. Allow microphone access on your desktop browser.<br/>2. Navigate to this URL on your mobile device: </h3>
-    	<h1>{window.location.href}</h1>
+const DesktopVRView = ({ override }) => (
+    <div style={styles.container}>
+    	<h3 style={styles.title}><strong>Welcome to StagedFright's VR speech practice mode!</strong></h3>
+    	<h4>1. Allow microphone access on your desktop browser.<br/>
+    		2. Navigate to the following URL on your VR-enabled mobile device:
+    	</h4>
+    	<h3><strong>{window.location.href}</strong></h3>
+    	<br/>
+    	<br/>
+    	<div onClick={override}>
+    		<h4>Alternatively, click the button below to override device check and proceed to VR speech practice in desktop mode.</h4>
+    	{/*TODO: fix button styling that cuts off label text*/}
+    		<MyNonSubmitButton />
+    	</div>
+    	<br/>
+    	<br/>
+    	<br/>
+    	<h4><strong>Having trouble enabling VR in your desktop browser?</strong></h4>
+    	<br/>
+    	<h5>Follow these instructions to <a href="https://superuser.com/questions/836832/how-can-i-enable-webgl-in-my-browser">enable WebGL and WebVR in your browser</a>.
+    	</h5> 
     </div>
 )
 
--- browser/react/components/DesktopVRView/styles/index.js
+++ browser/react/components/DesktopVRView/styles/index.js
@@ -1,3 +1,12 @@
 export default {
-
+	container: {
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		width: '90%',
+		paddingTop: '10vh',
+		textAlign: 'center',
+	},
+	title: {
+		fontFamily: 'Limelight'
+	}
 };
