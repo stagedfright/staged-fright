f2363073b37057ab1328a6070d4706d974b37ece
Jolanta Wolak <JTWolak@gmail.com>
2017-02-10 13:03:15 -0500

    added relevant information to the HowTo component

--- browser/react/components/HowTo/index.js
+++ browser/react/components/HowTo/index.js
@@ -14,7 +14,25 @@ class HowTo extends Component {
 
   render() {
     return (
-      <div style={styles.padded}>Here is some info on how to use It!!!!</div>
+      <div style={styles.padded}>
+        The speech you just entered on the previous page has been formatted for you and will be scrolled at the speed selected.
+        <br/>
+        In addition to the speech text there will also be 2 graphic indicators providing you with a visual feedback of your performace:
+        <br/>
+        <div className="col s6">
+          <ul className="list" style={styles.list}>
+            <li className="list" style={styles.list}><strong>Volume Bar</strong>: The volume bar will indicate how well you’re projecting your voice. Green will indicate that you’re speaking at a loud enough level.</li>
+            <li className="list" style={styles.list}><strong>Pitch Detector</strong>: The pitch detector looks at 10 second increments of your tone of voice, and determines whether or not you’re modulating your voice (green) or if you sound monotonous (yellow).</li>
+          </ul>
+          <br/>
+          <p>
+            <strong>Important Note</strong>: Whether or not you proceed on desktop or mobile, your voice will be recorded via your desktop browser. Therefore please stay near your desktop to get good results with real-time feedback!
+          </p>
+        </div>
+        <div className="col s6">
+          <img src="/screenshot.png" style={{ width: '100%', padding: '20px' }}/>
+        </div>
+      </div>
     );
   }
 }
